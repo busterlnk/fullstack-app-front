d60c1deb1484c6d41e8f5c48b70d260f3701a4d7
Busterlink <73650574+busterlnk@users.noreply.github.com>
2024-09-19 19:38:21 +0200

    Now can run in windows

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN npm install pm2@latest -g
 
 USER node
 
+EXPOSE 3000
+
 ENTRYPOINT ["bash", "/app/npm_starter.sh" ]
--- npm_starter.sh
+++ npm_starter.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-npm set @ingka:registry https://npm.m2.blue.cdtapps.com
-npm set @frontend:registry=http://nexus-repo.bitboxwebs.com:8081/repository/npmrepo-comun-bbx/
-npm set registry=https://registry.npmjs.org
-npm set //nexus-repo.bitboxwebs.com:8081/repository/npmrepo-comun-bbx/:_authToken=NpmToken.ceb7452e-cdd4-32ad-8806-46edcbeafbad
-
 # Procesar cada directorio que contenga un package.json
 for dir in */; do
     if [ -f "${dir}package.json" ]; then
